ece7fd38e4bd0b4092bdf5cc10481d86b2210eb9
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-01-20 12:06:08 +0000

    up

--- script.js
+++ script.js
@@ -4,3 +4,20 @@
 1 kilogram = 2.204 pound
 */
 
+
+// ID's to variables
+const number  = document.getElementById("number");
+const btn = document.getElementById("btn");
+const length = document.getElementById("Length");
+const volume = document.getElementById("Volume");
+const mass = document.getElementById("Mass");
+
+
+let convert = ()=> {
+    length.textContent = `${number.value} meters = ${number.value * 3.281.toFixed(2)}   ||   ${number.value} feet = ${number.value / 3.281.toFixed(2)} `
+
+}
+
+
+
+
