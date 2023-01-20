29837875e048c079d502000fafdfa32ce0eecedf
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-01-20 12:20:58 +0000

    final v1.0

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ const mass = document.getElementById("Mass");
 
 
 let convert = ()=> {
-    length.textContent = `${number.value} meters = ${number.value * 3.281.toFixed(2)}   ||   ${number.value} feet = ${number.value / 3.281.toFixed(2)} `
+    length.textContent = `${number.value} meters = ${parseFloat(number.value * 3.281).toFixed(1)} feet  ||   ${number.value} feet = ${parseFloat(number.value / 3.281).toFixed(1)} meters `
+    volume.textContent = `${number.value} liters = ${parseFloat(number.value * 0.264).toFixed(1)} gallons  ||   ${number.value} gallon = ${parseFloat(number.value / 0.264).toFixed(1)} liters `
+    mass.textContent = `${number.value} kilograms = ${parseFloat(number.value * 2.204).toFixed(1)} pounds   ||   ${number.value} pounds = ${parseFloat(number.value / 2.204).toFixed(1)} kilograms `
 
 }
 
